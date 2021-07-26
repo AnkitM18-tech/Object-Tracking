83194da67e38d3c69a06b0ce5c150a0d8967a2f9
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-07-26 20:00:49 +0530

    Project Completed

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import cv2
 from tracking import *
 
-cap = cv2.VideoCapture("highway.mp4")
+cap = cv2.VideoCapture("highway_bike.mp4")
+
+#create tracker object
+tracker = EuclideanDistTracker()
 
 #object detection from stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2(history = 100,varThreshold=30)
@@ -10,15 +13,16 @@ while True:
     ret,frame = cap.read()
 
     #Extract region of interest
-    height,width, _ = frame.shape
-    print(height,width)
+    # height,width, _ = frame.shape
+    # print(height,width)
 
-    roi = frame[100:480,100:700]
+    roi = frame[340:720,500:800]
 
     #object detection
     mask = object_detector.apply(roi)
     _ , mask = cv2.threshold(mask,254,255,cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
 
     for cnt in contours:
         #calculate area and remove small elements
@@ -26,7 +30,14 @@ while True:
         if area > 100:
             #cv2.drawContours(roi,[cnt],-1,(255,0,0),2)
             x,y,w,h = cv2.boundingRect(cnt)
-            cv2.rectangle(roi,(x,y),(x+w,y+h),(255,0,0),2)
+            detections.append([x,y,w,h])
+
+    #object tracking
+    boxes_id = tracker.update(detections)
+    for box_id in boxes_id:
+        x,y,w,h,id_ = box_id
+        cv2.putText(roi,str(id_),(x,y-15),cv2.FONT_HERSHEY_PLAIN,1,(0,0,255),2)
+        cv2.rectangle(roi,(x,y),(x+w,y+h),(255,0,0),2)
 
     cv2.imshow("Frame",frame)
     cv2.imshow("ROI",roi)
